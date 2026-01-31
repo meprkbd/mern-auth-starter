153447c8abcaa8195ab2e01c9a6981fd6ff0cbc7
meprkbd <meprkbd@gmail.com>
2026-01-31 21:28:02 +0600

    feat(error-handling): add different app errors

---
+++ backend/src/utils/catchErrors.ts
@@ -0,0 +1,49 @@
+import { HttpStatus, type HttpStatusCode } from "../config/http.config.js";
+import { ErrorCode } from "../enums/errorCode.js";
+import { AppError } from "./appError.js";
+
+export class NotFoundException extends AppError {
+  constructor(message = "Resource not found", errorCode?: ErrorCode) {
+    super(
+      message,
+      HttpStatus.NOT_FOUND,
+      errorCode || ErrorCode.RESOURCE_NOT_FOUND,
+    );
+  }
+}
+
+export class BadRequestException extends AppError {
+  constructor(message = "Bad Request", errorCode?: ErrorCode) {
+    super(message, HttpStatus.BAD_REQUEST, errorCode);
+  }
+}
+
+export class UnauthorizedException extends AppError {
+  constructor(message = "Unauthorized Access", errorCode?: ErrorCode) {
+    super(
+      message,
+      HttpStatus.UNAUTHORIZED,
+      errorCode || ErrorCode.ACCESS_UNAUTHORIZED,
+    );
+  }
+}
+
+export class InternalServerException extends AppError {
+  constructor(message = "Internal Server Error", errorCode?: ErrorCode) {
+    super(
+      message,
+      HttpStatus.INTERNAL_SERVER_ERROR,
+      errorCode || ErrorCode.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+export class HttpException extends AppError {
+  constructor(
+    message = "Http Exception Error",
+    statusCode: HttpStatusCode,
+    errorCode?: ErrorCode,
+  ) {
+    super(message, statusCode, errorCode);
+  }
+}
